4dcb9cf946cc3d77d73dbd47a4dc8534f05b9aa0
Lucas Aguiar <7lucasdaniel@gmail.com>
2024-08-24 14:14:52 -0300

    fix: comentando importações não utilizadas no projeto

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import Router from './router/routes'
 import { BrowserRouter } from 'react-router-dom'
 import { GlobalStyle } from './styles/global.ts'
 
-import AuthProvider from 'react-auth-kit';
-import createStore from 'react-auth-kit';
+// import AuthProvider from 'react-auth-kit';
+// import createStore from 'react-auth-kit';
 
 
 export default function App() {
